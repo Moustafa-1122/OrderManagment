3005772d1d9ae2a6389e5d4d15bd1acdf148b1e6
Moustafa <moustafa.mariam29@gmail.com>
2026-09-19 13:46:40 +0300

    add Models

---
+++ src/model/item.model.ts
@@ -0,0 +1,8 @@
+export interface Item {
+    getCategory():itemCategory;
+}
+
+export enum itemCategory {
+    CAKE,
+    
+}
---
+++ src/model/order.model.ts
@@ -0,0 +1,11 @@
+import { Item } from "./item.model";
+
+
+
+export interface Order {
+    getItem(): Item;
+    getQuantity(): number;
+    getPrice(): number;
+    getId(): string;
+
+}
